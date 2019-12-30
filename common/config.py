ad62ce22d815fbee9c16b235a94f2ae7d95f8f8a
zwpzwp123 <947273361@qq.com>
2019-12-30 17:32:52 +0800

    手机验证

---
+++ common/config.py
@@ -0,0 +1,9 @@
+SMS_SENDSMS = "https://open.ucpaas.com/ol/sms/{function}"
+MOBILE_NONE_SMS = {
+    "sid": 'd003afa86b13be853e57b6031c8cf02f',
+    "token": '9bff067b54a88f7c0b97bff69f518498',
+    "appid": '45414ae0c0f64e25b5c3cde8cc2924d4',
+    "templateid": "487820",
+    "param": None,
+    "mobile": None,
+}
